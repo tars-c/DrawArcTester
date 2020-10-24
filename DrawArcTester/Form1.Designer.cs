6b8d5009dc3436e9703d846918fc089e2cc37b41
tars-c <securemem@icloud.com>
2020-10-24 17:58:48 +0900

    SRC Commit

--- DrawArcTester/Form1.Designer.cs
+++ DrawArcTester/Form1.Designer.cs
@@ -28,13 +28,314 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(46, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(95, 15);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(64, 25);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(95, 46);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(64, 25);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(46, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Location = new System.Drawing.Point(95, 89);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(64, 25);
+            this.numericUpDown3.TabIndex = 6;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(46, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Width";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Location = new System.Drawing.Point(95, 120);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(64, 25);
+            this.numericUpDown4.TabIndex = 8;
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(46, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Height";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.DecimalPlaces = 1;
+            this.numericUpDown5.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Location = new System.Drawing.Point(95, 162);
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(64, 25);
+            this.numericUpDown5.TabIndex = 10;
+            this.numericUpDown5.ValueChanged += new System.EventHandler(this.numericUpDown5_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(7, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Start°";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.DecimalPlaces = 1;
+            this.numericUpDown6.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Location = new System.Drawing.Point(95, 193);
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(64, 25);
+            this.numericUpDown6.TabIndex = 12;
+            this.numericUpDown6.ValueChanged += new System.EventHandler(this.numericUpDown6_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(7, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Sweep°";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.vScrollBar2);
+            this.panel1.Location = new System.Drawing.Point(177, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 358);
+            this.panel1.TabIndex = 13;
+            this.panel1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.panel1_Scroll);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.vScrollBar2.LargeChange = 50;
+            this.vScrollBar2.Location = new System.Drawing.Point(584, 0);
+            this.vScrollBar2.Maximum = 2000;
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(16, 358);
+            this.vScrollBar2.SmallChange = 50;
+            this.vScrollBar2.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 335);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 356);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 17);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "label8";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(95, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 18);
+            this.button1.TabIndex = 16;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(7, 236);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 23);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Stroke";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(7, 264);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(82, 23);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Rect";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(95, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(20, 18);
+            this.button2.TabIndex = 18;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Location = new System.Drawing.Point(85, 307);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(86, 72);
+            this.listBox1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(789, 385);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
     }
 }
 
--- DrawArcTester/Form1.cs
+++ DrawArcTester/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -11,9 +12,200 @@ namespace DrawArcTester
 {
     public partial class Form1 : Form
     {
+        public int ArcX { get; set; }
+        public int ArcY { get; set; }
+        public int ArcWidth { get; set; }
+        public int ArcHeight { get; set; }
+        public float ArcStartAngle { get; set; }
+        public float ArcSweepAngle { get; set; }
+
+        private NumericUpDown[] list;
+        private decimal[] property_list;
+
+        public Color ArcStrokeColor { get; set; }
+        public Color ArcRectColor { get; set; }
+
+        public int ArcStrokeSize { get; set; }
+        public int ArcRectSize { get; set; }
+
+        private int prevScrollValue { get; set; }
+
+
+
         public Form1()
         {
             InitializeComponent();
+            
+            list = new NumericUpDown[]
+            {
+                numericUpDown1,
+                numericUpDown2,
+                numericUpDown3,
+                numericUpDown4,
+                numericUpDown5,
+                numericUpDown6,
+            };
+
+            property_list = new decimal[]
+            {
+                Convert.ToDecimal(ArcX = 0),
+                Convert.ToDecimal(ArcY = 0),
+                Convert.ToDecimal(ArcWidth = 300),
+                Convert.ToDecimal(ArcHeight = 300),
+                Convert.ToDecimal(ArcStartAngle = 0.0f),
+                Convert.ToDecimal(ArcSweepAngle = 360.0f),
+            };
+            ArcStrokeColor = Color.Black;
+            button1.BackColor = ArcStrokeColor;
+
+            for (uint i=0; i<list.Length; ++i)
+            {
+                list[i].Maximum = 3000m;
+                list[i].Text = property_list[i].ToString();
+            }
+
+            //panel1.Width = 300;
+            //panel1.Height = 300;
+            //panel1.AutoScrollMinSize = new Size(1500, 1500);
+            vScrollBar2.Value = 0;
+            vScrollBar2.Minimum = 0;
+            vScrollBar2.SmallChange = ((vScrollBar2.Maximum - vScrollBar2.Minimum) / 40);
+            vScrollBar2.LargeChange = ((vScrollBar2.Maximum - vScrollBar2.Minimum) / 40);
+            vScrollBar2.Maximum = 2000 + (vScrollBar2.LargeChange - 1);
+            vScrollBar2.Scroll += (sender, e) => {
+
+                if (prevScrollValue != vScrollBar2.Value)
+                {
+                    panel1.Invalidate();
+                }
+                prevScrollValue = vScrollBar2.Value;
+            };
+            ArcStrokeSize = 3;
+            ArcRectSize = 3;
+            panel1.Controls.Add(vScrollBar2);   
+
+            panel1.MouseEnter += (s, e) => panel1.Focus();
+            this.panel1.MouseWheel += (s, e) => {
+                // Debug
+                //Console.Out.WriteLine(e.Delta);
+                //listBox1.Items.Insert(0, vScrollBar2.Value + "," + -(e.Delta / 120) * vScrollBar2.SmallChange);
+                
+                // 델타값 계산
+                int v = ((((-e.Delta) / 120) * vScrollBar2.SmallChange) / 2); 
+                if (v + vScrollBar2.Value < 0)
+                {
+                    vScrollBar2.Value = 0;
+                } else if (v + vScrollBar2.Value > (vScrollBar2.Maximum - (vScrollBar2.LargeChange)) + 1)
+                {
+                    vScrollBar2.Value = (vScrollBar2.Maximum - (vScrollBar2.LargeChange))+1;
+                } else
+                {
+                    vScrollBar2.Value += v;
+                }
+
+                if (prevScrollValue != vScrollBar2.Value)
+                {
+                    panel1.Invalidate();
+                }
+                prevScrollValue = vScrollBar2.Value;
+            };
+                
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+            label7.Text = vScrollBar2.Value + ": Hori";
+            label8.Text = vScrollBar2.Value + ": Verti";
+
+            Pen p = new Pen(ArcStrokeColor, ArcStrokeSize);
+
+                GraphicsPath path = new GraphicsPath();
+                Rectangle rect = new Rectangle((ArcX+ ArcRectSize) - panel1.HorizontalScroll.Value,
+                                (ArcY + ArcRectSize) - vScrollBar2.Value,
+                                ArcWidth,
+                                ArcHeight);
+
+            e.Graphics.DrawArc(p, rect,
+                      ArcStartAngle, ArcSweepAngle);
+
+
+            path.AddRectangle(rect);
+                e.Graphics.DrawPath(new Pen(ArcRectColor, ArcRectSize), path);
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            ArcX = Decimal.ToInt32((sender as NumericUpDown).Value);
+            panel1.Invalidate();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            ArcY = Decimal.ToInt32((sender as NumericUpDown).Value);
+            panel1.Invalidate();
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            ArcWidth = Decimal.ToInt32((sender as NumericUpDown).Value);
+            panel1.Invalidate();
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            ArcHeight = Decimal.ToInt32((sender as NumericUpDown).Value);
+            panel1.Invalidate();
+        }
+
+        private void numericUpDown5_ValueChanged(object sender, EventArgs e)
+        {
+            ArcStartAngle = Decimal.ToSingle((sender as NumericUpDown).Value);
+            if (ArcStartAngle > 360.0)
+            {
+                (sender as NumericUpDown).Value = 0.0m;
+            }
+            panel1.Invalidate();
+        }
+
+        private void numericUpDown6_ValueChanged(object sender, EventArgs e)
+        {
+            ArcSweepAngle = Decimal.ToSingle((sender as NumericUpDown).Value);
+            if (ArcSweepAngle > 360.0)
+            {
+                (sender as NumericUpDown).Value = 0.0m;
+            }
+            panel1.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.FullOpen = true;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ArcStrokeColor = colorDialog1.Color;
+                button1.BackColor = ArcStrokeColor;
+                panel1.Invalidate();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.FullOpen = true;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                ArcRectColor = colorDialog1.Color;
+                button2.BackColor = ArcRectColor;
+                panel1.Invalidate();
+            }
+        }
+        static int i = 0;
+
+        private void panel1_Scroll(object sender, ScrollEventArgs e)
+        {
+            vScrollBar2.Focus();
         }
     }
 }
